d0d86db148f0dcbb4a59e237470b27cbbf8bef7a
Manpreet Sokhi <manpreetsokhi99@gmail.com>
2020-09-11 03:29:25 -0700

    added drag and drop

--- EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -19,13 +19,60 @@ struct EmojiArtDocumentView: View {
                     ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
                         Text(emoji)
                             .font(Font.system(size: self.defaultEmojiSize))
+                            .onDrag { return NSItemProvider(object: emoji as NSString) } // old objc code
                     }
                 }
             }
             .padding(.horizontal)
-            Rectangle().foregroundColor(.yellow)
-                .edgesIgnoringSafeArea([.horizontal, .bottom])
+            GeometryReader { geometry in
+                ZStack {
+                    // overlay rather than ZStack because of size
+                    Color.white.overlay(
+                        Group {
+                            if self.document.backgroundImage != nil {
+                                Image(uiImage: self.document.backgroundImage!)
+                            }
+                        }
+                    )
+                        .edgesIgnoringSafeArea([.horizontal, .bottom])
+                        // of - what kind of thing do you want to drop / "public.iamge" - a URI that specifies type of things that are images
+                        // isTargeted - lets us know when user is dragging something over NOT when dropping
+                        // function/closure - providers - provide information that is being dropped (transfer needs to happen asynchronously) / location - to drop
+                        .onDrop(of: ["public.image"], isTargeted: nil) { providers, location in
+                            var location = geometry.convert(location, from: .global)
+                            location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                            // return if drop succeeds
+                            return self.drop(providers: providers, at: location)
+                        }
+                    ForEach(self.document.emojis) { emoji in
+                        Text(emoji.text)
+                            .font(self.font(for: emoji))
+                            .position(self.position(for: emoji, in: geometry.size))
+                    }
+                }
+            }
         }
+    }
+    
+    private func font(for emoji: EmojiArt.Emoji) -> Font {
+        Font.system(size: emoji.fontSize)
+    }
+    
+    private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
+        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+    }
+    
+    private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
+        var found = providers.loadFirstObject(ofType: URL.self) { url in
+            print("dropped \(url)")
+            self.document.setBackgroundURL(url)
+        }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                self.document.addEmoji(string, at: location, size: self.defaultEmojiSize)
+            }
+        }
+        return found
     }
     
     private let defaultEmojiSize: CGFloat = 40
--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -15,6 +15,11 @@ class EmojiArtDocument: ObservableObject {
     // @Published because everytime emojiArt changes need to use ObservableObject to cause View to redraw
     @Published private var emojiArt: EmojiArt = EmojiArt()
     
+    // UIImage is actaully from UIKit but works well and ? because we may have link but not UIImage
+    @Published private(set) var backgroundImage: UIImage?
+    
+    var emojis: [EmojiArt.Emoji] { emojiArt.emojis } // return emojiArt - a read only access to model
+    
     // MARK: - Intent(s)
     
     func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
@@ -36,5 +41,30 @@ class EmojiArtDocument: ObservableObject {
     
     func setBackgroundURL(_ url: URL?) {
         emojiArt.backgroundURL = url?.imageURL
+        
+        // only job is to set the backgroundImage, ReactiveUI is set
+        fetchBackgroundImageData()
     }
+    
+    // would use URL Sessions if really downloading data
+    private func fetchBackgroundImageData() {
+        backgroundImage = nil
+        if let url = self.emojiArt.backgroundURL {
+            DispatchQueue.global(qos: .userInitiated).async { // do this on a background thread
+                if let imageData = try? Data(contentsOf: url) { // this could take variable time, could freeze our app - use dispatch mechanism
+                    DispatchQueue.main.async {
+                        if url == self.emojiArt.backgroundURL { // error handling to make sure image requested is still the right one
+                            self.backgroundImage = UIImage(data: imageData) // if outside of main - problem here is that this forces View to redraw BUT on a background thread - NOT ALLOWED
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// interpretation of model - now we don't have to deal with Ints in View
+extension EmojiArt.Emoji {
+    var fontSize: CGFloat { CGFloat(self.size) }
+    var location: CGPoint { CGPoint(x: CGFloat(x), y: CGFloat(y)) }
 }
